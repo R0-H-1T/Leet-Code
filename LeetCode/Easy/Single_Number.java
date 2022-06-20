ba3d3e01f2467a50317508d271c1d58af3d4bb4f
Rohit Tank <rowhittank@gmail.com>
2022-06-20 22:38:10 +0530

    Bitwise Operator

---
+++ LeetCode/Easy/Single_Number.java
@@ -0,0 +1,13 @@
+public class Single_Number {
+    public int singleNumber(int[] nums) {
+        //XOR Bitwise operator
+        //0 ^ N = N
+        //N ^ N = 0
+        int res=0;
+        for(int n:nums){
+            res^=n;
+        }
+        return res;        
+
+    } 
+}
