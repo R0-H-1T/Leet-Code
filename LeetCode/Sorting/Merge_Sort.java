53bee456b3192b102eb6a415e6dd4786716478b7
Rohit Tank <rowhittank@gmail.com>
2022-06-23 11:28:56 +0530

    merge sort

--- LeetCode/Sorting/Merge_Sort.java
+++ LeetCode/Sorting/Merge_Sort.java
@@ -1,22 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 class Merge_Sort{
-    public void mergeSort(int[] arr, int l, int r){
-        
+    public static int[] sort(int[] arr){
+        if(arr.length == 1) return arr;
+
+        int mid = arr.length/2;
+        int[] left = sort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = sort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
     }
-    public static void sort(int[] arr, int l, int r){
-        if(l<r){
-            
-            int mid = (l+r)/2;
-            sort(arr, l, mid);
-            sort(arr, mid+1, r);
+    public static int[] merge(int[] first, int[] second){
+        int[] newArr = new int[first.length + second.length];
+        int i=0, j=0, k=0;
+        while(i<first.length && j<second.length){
+            if(first[i]<second[j]) newArr[k] = first[i++];
+            else newArr[k] = second[j++];
+            k++;
         }
+        while(i<first.length) newArr[k++] = first[i++];
+        while(j<second.length) newArr[k++] = second[j++];
 
-    }
-    public void printArray(int[] arr){
-
+        return newArr;
     }
     public static void main(String[] args) {
         int[] nums = {12,11,7,5,6};
-        sort(nums, 0, nums.length-1);
+        nums  = sort(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
 
